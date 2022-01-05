ac089eb1767c9adf0d0a259e91d68e54026ecf24
Zackery Arsement <zarsement@hotmail.com>
2022-01-04 19:04:42 -0600

    You can go through the quiz and keep track if you selected the correct answer or not.

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ var arrayOfQuestions = [
         'question': 'What character is typically placed at the end of each segment of javascript code?',
         'firstAnswer': '1: /',
         'secondAnswer': '2: ,',
-        'thirddAnswer': '3: ;',
+        'thirdAnswer': '3: ;',
         'fourthAnswer': '4: :',
         'correctAnswer': 3
     },
@@ -29,7 +29,7 @@ var arrayOfQuestions = [
         'question': 'Commonly used data types DO NOT include: ',
         'firstAnswer': '1: boolean',
         'secondAnswer': '2: length',
-        'thirddAnswer': '3: string',
+        'thirdAnswer': '3: string',
         'fourthAnswer': '4: integer',
         'correctAnswer': 2
     },
@@ -38,7 +38,7 @@ var arrayOfQuestions = [
         'question': 'Which JavaScript variable is using standard variable name syntax?',
         'firstAnswer': '1: colorArray',
         'secondAnswer': '2: 3rdperson',
-        'thirddAnswer': '3: GreenTree',
+        'thirdAnswer': '3: GreenTree',
         'fourthAnswer': '4: tropicaLbeach',
         'correctAnswer': 1
     },
@@ -47,7 +47,7 @@ var arrayOfQuestions = [
         'question': 'If you would like to display a message in the webpage console, what method would you use?',
         'firstAnswer': '1: show.text()',
         'secondAnswer': '2: print()',
-        'thirddAnswer': '3: display()',
+        'thirdAnswer': '3: display()',
         'fourthAnswer': '4: console.log()',
         'correctAnswer': 4
     },
@@ -56,7 +56,7 @@ var arrayOfQuestions = [
         'question': 'If I wanted to apply changes to every value within an array, what would be the best way to do so?',
         'firstAnswer': '1: Use a for loop',
         'secondAnswer': '2: Apply the change to each value individually, line by line',
-        'thirddAnswer': '3: Use an if/else statement',
+        'thirdAnswer': '3: Use an if/else statement',
         'fourthAnswer': '4: Array.apply()',
         'correctAnswer': 1
     },
@@ -65,14 +65,13 @@ var arrayOfQuestions = [
         'question': 'What is the third primary language used in web developement other than html and javascript?',
         'firstAnswer': '1: C#',
         'secondAnswer': '2: css',
-        'thirddAnswer': '3: jquery',
+        'thirdAnswer': '3: jquery',
         'fourthAnswer': '4: react',
         'correctAnswer': 2
     }
 ]
 
 startButton.addEventListener('click', function(event){
-    quizOn = true;
     startButton.style.display = 'none';
     answer1.style.display = 'block';
     answer2.style.display = 'block';
@@ -82,56 +81,56 @@ startButton.addEventListener('click', function(event){
 })
 
 function nextQuestion(){
-    questionCount++;
-
-    console.log(arrayOfQuestions[questionCount].correctAnswer);
+    questionCount = questionCount + 1;
+    console.log(questionCount);
 
     description.textContent = arrayOfQuestions[questionCount].question;
     answer1.textContent = arrayOfQuestions[questionCount].firstAnswer;
     answer2.textContent = arrayOfQuestions[questionCount].secondAnswer;
-    answer3.textContent = arrayOfQuestions[questionCount].thirddAnswer;
+    answer3.textContent = arrayOfQuestions[questionCount].thirdAnswer;
     answer4.textContent = arrayOfQuestions[questionCount].fourthAnswer;
-
-    answer1.addEventListener('click', function(){
-        if(arrayOfQuestions[questionCount].correctAnswer === 1){
-            console.log("Correct");
-            nextQuestion();
-        }
-        else{
-            console.log("Wrong");
-            nextQuestion();
-        }
-    })
-
-    answer2.addEventListener('click', function(){
-        if(arrayOfQuestions[questionCount].correctAnswer === 2){
-            console.log("Correct");
-            nextQuestion();
-        }
-        else{
-            console.log("Wrong");
-            nextQuestion();
-        }
-    })
-
-    answer3.addEventListener('click', function(){
-        if(arrayOfQuestions[questionCount].correctAnswer === 3){
-            nextQuestion();
-        }
-        else{
-            console.log("Wrong");
-            nextQuestion();
-        }
-    })
-
-    answer4.addEventListener('click', function(){
-        if(arrayOfQuestions[questionCount].correctAnswer === 4){
-            console.log("Correct");
-            nextQuestion();
-        }
-        else{
-            console.log("Wrong");
-            nextQuestion();
-        }
-    })
 }
+
+answer1.addEventListener('click', function(){
+    if(arrayOfQuestions[questionCount].correctAnswer === 1){
+        console.log("Correct 1");
+    }
+    else{
+        console.log("Wrong 1");
+    }
+
+    nextQuestion();
+});
+
+answer2.addEventListener('click', function(){
+    if(arrayOfQuestions[questionCount].correctAnswer === 2){
+        console.log("Correct 2");
+    }
+    else{
+        console.log("Wrong 2");
+    }
+
+    nextQuestion();
+});
+
+answer3.addEventListener('click', function(){
+    if(arrayOfQuestions[questionCount].correctAnswer === 3){
+        console.log("Correct 3");
+    }
+    else{
+        console.log("Wrong 3");
+    }
+
+    nextQuestion();
+});
+
+answer4.addEventListener('click', function(){
+    if(arrayOfQuestions[questionCount].correctAnswer === 4){
+        console.log("Correct 4");
+    }
+    else{
+        console.log("Wrong 4");
+    }
+
+    nextQuestion();
+});
