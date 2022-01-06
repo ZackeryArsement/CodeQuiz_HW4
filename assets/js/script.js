ce0f4a2fbe1242e20371d08b0050cb4ad8b07e06
Zackery Arsement <zarsement@hotmail.com>
2022-01-06 13:49:37 -0600

    The quiz is done and input is added once you complete the quiz. I still need to add the highscore list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var startButton = document.querySelector('#start-button');
 var description = document.querySelector('#main-screen-description');
+var timerDisplay = document.querySelector('#timer');
+var resultDisplay = document.querySelector('#result');
+var title = document.querySelector('#title');
+var input = document.querySelector('#input-id');
 
 var buttons = document.querySelectorAll('.buttons');
 
@@ -8,13 +12,17 @@ var answer2 = document.querySelector('#answer-2');
 var answer3 = document.querySelector('#answer-3');
 var answer4 = document.querySelector('#answer-4');
 
+var secondsLeft = 60;
+
 answer1.style.display = 'none';
 answer2.style.display = 'none';
 answer3.style.display = 'none';
 answer4.style.display = 'none';
 
 var questionCount = -1;
+var timerInterval;
 
+// An array of objects for each question for the quiz
 var arrayOfQuestions = [
     {
         'question': 'What character is typically placed at the end of each segment of javascript code?',
@@ -71,6 +79,7 @@ var arrayOfQuestions = [
     }
 ]
 
+// By pressing the start button you make all the question buttons visible and make the start button not visible
 startButton.addEventListener('click', function(event){
     startButton.style.display = 'none';
     answer1.style.display = 'block';
@@ -78,59 +87,137 @@ startButton.addEventListener('click', function(event){
     answer3.style.display = 'block';
     answer4.style.display = 'block';
     nextQuestion();
+    setTime();
 })
 
-function nextQuestion(){
-    questionCount = questionCount + 1;
-    console.log(questionCount);
+// Checks to see if answer 1 is the correct answer
+answer1.addEventListener('click', function(){
+    resultDisplay.style.display = 'block';
 
-    description.textContent = arrayOfQuestions[questionCount].question;
+    if(arrayOfQuestions[questionCount].correctAnswer === 1){
+        resultDisplay.textContent = 'Correct!';
+        displayResult();
+    }
+    else{
+        resultDisplay.textContent = 'Wrong!';
+        secondsLeft = secondsLeft - 15;
+        displayResult();
+    }
+
+    nextQuestion();
+});
+
+// Checks to see if answer 2 is the correct answer
+answer2.addEventListener('click', function(){
+    resultDisplay.style.display = 'block';
+
+    if(arrayOfQuestions[questionCount].correctAnswer === 2){
+        resultDisplay.textContent = 'Correct!';
+        displayResult();
+    }
+    else{
+        resultDisplay.textContent = 'Wrong!';
+        secondsLeft = secondsLeft - 15;
+        displayResult();
+    }
+
+    nextQuestion();
+});
+
+// Checks to see if answer 3 is the correct answer
+answer3.addEventListener('click', function(){
+    resultDisplay.style.display = 'block';
+
+    if(arrayOfQuestions[questionCount].correctAnswer === 3){
+        resultDisplay.textContent = 'Correct!';
+        displayResult();
+    }
+    else{
+        resultDisplay.textContent = 'Wrong!';
+        secondsLeft = secondsLeft - 15;
+        displayResult();
+    }
+
+    nextQuestion();
+});
+
+// Checks to see if answer 4 is the correct answer
+answer4.addEventListener('click', function(){
+    resultDisplay.style.display = 'block';
+
+    if(arrayOfQuestions[questionCount].correctAnswer === 4){
+        resultDisplay.textContent = 'Correct!';
+        displayResult();
+    }
+    else{
+        resultDisplay.textContent = 'Wrong!';
+        secondsLeft = secondsLeft - 15;
+        displayResult();
+    }
+
+    nextQuestion();
+});
+
+// Run a timer while the quiz is going and display the time left for the user in the nav bar
+function setTime(){
+    title.style.display = 'none';
+
+    timerInterval = setInterval(function(){
+        secondsLeft--;
+        timerDisplay.textContent = 'Time Left: ' + secondsLeft;
+
+        if(secondsLeft <= 0 && questionCount !== 4){
+            endQuiz();
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+// Display if the answer you selected was correct or wrong at the bottom of the screen
+function displayResult(){
+    var resultTime = 2;
+
+    var resultTimer = setInterval(() => {
+        resultTime--;
+
+        if(resultTime === 0){
+            resultDisplay.style.display = 'none';
+            clearInterval(resultTimer);
+        }
+    }, 750);
+}
+
+// Grab the next object out of the array of questions, to grab the new question and answers
+function nextQuestion(){
+
+    if(questionCount === 4){
+        endQuiz();
+        clearInterval(timerInterval);
+        timerDisplay.textContent = 'Time Left: ' + 0;
+    }
+    else{
+        questionCount = questionCount + 1;
+        description.textContent = arrayOfQuestions[questionCount].question;
+    }
+
     answer1.textContent = arrayOfQuestions[questionCount].firstAnswer;
     answer2.textContent = arrayOfQuestions[questionCount].secondAnswer;
     answer3.textContent = arrayOfQuestions[questionCount].thirdAnswer;
     answer4.textContent = arrayOfQuestions[questionCount].fourthAnswer;
 }
 
-answer1.addEventListener('click', function(){
-    if(arrayOfQuestions[questionCount].correctAnswer === 1){
-        console.log("Correct 1");
-    }
-    else{
-        console.log("Wrong 1");
-    }
+// Show the user their score and notify them that the game is over
+function endQuiz(){
+    title.textContent = 'All Done!';
+    title.style.display = 'block';
+    description.textContent = 'Your final score is ' + secondsLeft + '... Enter your name to keep track of your highscores!';
+    input.classList.remove('invisible');
+    input.classList.add('visible');
 
-    nextQuestion();
-});
 
-answer2.addEventListener('click', function(){
-    if(arrayOfQuestions[questionCount].correctAnswer === 2){
-        console.log("Correct 2");
-    }
-    else{
-        console.log("Wrong 2");
-    }
-
-    nextQuestion();
-});
-
-answer3.addEventListener('click', function(){
-    if(arrayOfQuestions[questionCount].correctAnswer === 3){
-        console.log("Correct 3");
-    }
-    else{
-        console.log("Wrong 3");
-    }
-
-    nextQuestion();
-});
-
-answer4.addEventListener('click', function(){
-    if(arrayOfQuestions[questionCount].correctAnswer === 4){
-        console.log("Correct 4");
-    }
-    else{
-        console.log("Wrong 4");
-    }
-
-    nextQuestion();
-});
+    answer1.style.display = 'none';
+    answer2.style.display = 'none';
+    answer3.style.display = 'none';
+    answer4.style.display = 'none';
+}
